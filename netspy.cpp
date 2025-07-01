9bbaa7ee27f0ccab077d8f1c89d897000b265d90
Dmitry Mikushin <dmitry@kernelgen.org>
2025-07-01 06:22:31 +0200

    Adding IPv6 support

--- netspy.cpp
+++ netspy.cpp
@@ -25,6 +25,7 @@
 #include <pcap.h>
 #include <stdarg.h>
 #include <random>
+#include <netinet/ip6.h>
 
 #include "network_interceptor.hpp"
 
@@ -121,75 +122,100 @@ int NetworkInterceptor::generatePacket(unsigned char* buffer, size_t bufferSize,
                                     const struct sockaddr_storage* srcAddr, 
                                     const struct sockaddr_storage* dstAddr,
                                     const void* data, size_t dataLen, int protocol) {
-    struct ip* ipHeader;
-    struct tcphdr* tcpHeader;
-    struct udphdr* udpHeader;
-    int headerLen = 0;
-    
     if (!buffer || bufferSize < sizeof(struct ip) + 
         (protocol == IPPROTO_TCP ? sizeof(struct tcphdr) : sizeof(struct udphdr)) + dataLen)
         return -1;
-    
-    // Only handle IPv4 for now
-    if (srcAddr->ss_family != AF_INET || dstAddr->ss_family != AF_INET)
-        return -1;
-    
-    // IP header
-    ipHeader = (struct ip*)buffer;
-    memset(ipHeader, 0, sizeof(struct ip));
-    
-    ipHeader->ip_v = 4;
-    ipHeader->ip_hl = sizeof(struct ip) >> 2;
-    
-    // Use random for id to avoid predictable sequence
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 0xFFFF);
-    
-    ipHeader->ip_id = htons(dis(gen));
-    ipHeader->ip_ttl = 64;
-    ipHeader->ip_p = protocol;
-    ipHeader->ip_src.s_addr = ((struct sockaddr_in*)srcAddr)->sin_addr.s_addr;
-    ipHeader->ip_dst.s_addr = ((struct sockaddr_in*)dstAddr)->sin_addr.s_addr;
-    
-    headerLen += sizeof(struct ip);
-    
-    // Transport header
-    if (protocol == IPPROTO_TCP) {
-        tcpHeader = (struct tcphdr*)(buffer + headerLen);
-        memset(tcpHeader, 0, sizeof(struct tcphdr));
-        
-        tcpHeader->th_sport = ((struct sockaddr_in*)srcAddr)->sin_port;
-        tcpHeader->th_dport = ((struct sockaddr_in*)dstAddr)->sin_port;
-        tcpHeader->th_seq = htonl(dis(gen));
-        tcpHeader->th_off = sizeof(struct tcphdr) >> 2;
-        tcpHeader->th_flags = TH_PUSH | TH_ACK;
-        tcpHeader->th_win = htons(65535);
-        
-        headerLen += sizeof(struct tcphdr);
-    } else if (protocol == IPPROTO_UDP) {
-        udpHeader = (struct udphdr*)(buffer + headerLen);
-        memset(udpHeader, 0, sizeof(struct udphdr));
-        
-        udpHeader->uh_sport = ((struct sockaddr_in*)srcAddr)->sin_port;
-        udpHeader->uh_dport = ((struct sockaddr_in*)dstAddr)->sin_port;
-        udpHeader->uh_ulen = htons(sizeof(struct udphdr) + dataLen);
-        
-        headerLen += sizeof(struct udphdr);
+
+    if (srcAddr->ss_family == AF_INET && dstAddr->ss_family == AF_INET) {
+        // IPv4 handling (existing code)
+        struct ip* ipHeader = (struct ip*)buffer;
+        struct tcphdr* tcpHeader;
+        struct udphdr* udpHeader;
+        int headerLen = 0;
+        memset(ipHeader, 0, sizeof(struct ip));
+        ipHeader->ip_v = 4;
+        ipHeader->ip_hl = sizeof(struct ip) >> 2;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 0xFFFF);
+        ipHeader->ip_id = htons(dis(gen));
+        ipHeader->ip_ttl = 64;
+        ipHeader->ip_p = protocol;
+        ipHeader->ip_src.s_addr = ((struct sockaddr_in*)srcAddr)->sin_addr.s_addr;
+        ipHeader->ip_dst.s_addr = ((struct sockaddr_in*)dstAddr)->sin_addr.s_addr;
+        headerLen += sizeof(struct ip);
+        if (protocol == IPPROTO_TCP) {
+            tcpHeader = (struct tcphdr*)(buffer + headerLen);
+            memset(tcpHeader, 0, sizeof(struct tcphdr));
+            tcpHeader->th_sport = ((struct sockaddr_in*)srcAddr)->sin_port;
+            tcpHeader->th_dport = ((struct sockaddr_in*)dstAddr)->sin_port;
+            tcpHeader->th_seq = htonl(dis(gen));
+            tcpHeader->th_off = sizeof(struct tcphdr) >> 2;
+            tcpHeader->th_flags = TH_PUSH | TH_ACK;
+            tcpHeader->th_win = htons(65535);
+            headerLen += sizeof(struct tcphdr);
+        } else if (protocol == IPPROTO_UDP) {
+            udpHeader = (struct udphdr*)(buffer + headerLen);
+            memset(udpHeader, 0, sizeof(struct udphdr));
+            udpHeader->uh_sport = ((struct sockaddr_in*)srcAddr)->sin_port;
+            udpHeader->uh_dport = ((struct sockaddr_in*)dstAddr)->sin_port;
+            udpHeader->uh_ulen = htons(sizeof(struct udphdr) + dataLen);
+            headerLen += sizeof(struct udphdr);
+        } else {
+            return -1;  // Unsupported protocol
+        }
+        if (data && dataLen > 0) {
+            memcpy(buffer + headerLen, data, dataLen);
+            headerLen += dataLen;
+        }
+        ipHeader->ip_len = htons(headerLen);
+        return headerLen;
+    } else if (srcAddr->ss_family == AF_INET6 && dstAddr->ss_family == AF_INET6) {
+        // IPv6 handling
+        struct ip6_hdr* ip6Header = (struct ip6_hdr*)buffer;
+        struct tcphdr* tcpHeader;
+        struct udphdr* udpHeader;
+        int headerLen = 0;
+        memset(ip6Header, 0, sizeof(struct ip6_hdr));
+        ip6Header->ip6_flow = htonl((6 << 28) | (0 << 20) | 0); // version, traffic class, flow label
+        ip6Header->ip6_plen = htons((protocol == IPPROTO_TCP ? sizeof(struct tcphdr) : sizeof(struct udphdr)) + dataLen);
+        ip6Header->ip6_nxt = protocol;
+        ip6Header->ip6_hops = 64;
+        ip6Header->ip6_src = ((struct sockaddr_in6*)srcAddr)->sin6_addr;
+        ip6Header->ip6_dst = ((struct sockaddr_in6*)dstAddr)->sin6_addr;
+        headerLen += sizeof(struct ip6_hdr);
+        if (protocol == IPPROTO_TCP) {
+            tcpHeader = (struct tcphdr*)(buffer + headerLen);
+            memset(tcpHeader, 0, sizeof(struct tcphdr));
+            tcpHeader->th_sport = ((struct sockaddr_in6*)srcAddr)->sin6_port;
+            tcpHeader->th_dport = ((struct sockaddr_in6*)dstAddr)->sin6_port;
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> dis(0, 0xFFFF);
+            tcpHeader->th_seq = htonl(dis(gen));
+            tcpHeader->th_off = sizeof(struct tcphdr) >> 2;
+            tcpHeader->th_flags = TH_PUSH | TH_ACK;
+            tcpHeader->th_win = htons(65535);
+            headerLen += sizeof(struct tcphdr);
+        } else if (protocol == IPPROTO_UDP) {
+            udpHeader = (struct udphdr*)(buffer + headerLen);
+            memset(udpHeader, 0, sizeof(struct udphdr));
+            udpHeader->uh_sport = ((struct sockaddr_in6*)srcAddr)->sin6_port;
+            udpHeader->uh_dport = ((struct sockaddr_in6*)dstAddr)->sin6_port;
+            udpHeader->uh_ulen = htons(sizeof(struct udphdr) + dataLen);
+            headerLen += sizeof(struct udphdr);
+        } else {
+            return -1; // Unsupported protocol
+        }
+        if (data && dataLen > 0) {
+            memcpy(buffer + headerLen, data, dataLen);
+            headerLen += dataLen;
+        }
+        return headerLen;
     } else {
-        return -1;  // Unsupported protocol
+        // Unsupported or mismatched address families
+        return -1;
     }
-    
-    // Copy data
-    if (data && dataLen > 0) {
-        memcpy(buffer + headerLen, data, dataLen);
-        headerLen += dataLen;
-    }
-    
-    // Set IP total length
-    ipHeader->ip_len = htons(headerLen);
-    
-    return headerLen;
 }
 
 void NetworkInterceptor::logPacketToPcap(const struct sockaddr_storage* srcAddr, 
